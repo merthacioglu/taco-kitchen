d211b07c862f3cf7e694fe030a7db6a6b7191f0d
Mert Hacioglu <mhacioglu96@gmail.com>
2024-09-21 21:12:38 +0100

    Initial commit

---
+++ src/test/java/org/mhacioglu/tacokitchen/TacoKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.tacokitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoKitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
